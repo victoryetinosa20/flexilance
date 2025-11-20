0f69a8de54b170d33c420ee0902ac24e34b30e1b
Developer <developer@example.com>
2025-11-20 18:40:53 +0100

    Remove collectstatic command from build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,5 @@ set -o errexit
 # Install project dependencies
 pip install -r requirements.txt
 
-# Collect static files for deployment
-python flexilance-mvp/manage.py collectstatic --no-input
-
 # Apply database migrations
 python flexilance-mvp/manage.py migrate
